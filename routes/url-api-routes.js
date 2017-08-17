180e24493aedd9aef4adbf6d7d7229ccd6cfaf77
julietshen <julietqshen@gmail.com>
2017-08-16 21:31:06 -0700

    disregard css and js files. Added CSS file and adjusted folder structures

--- routes/url-api-routes.js
+++ routes/url-api-routes.js
@@ -1,16 +1,16 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  app.get("/api/users", function(req, res) {
+  app.get("/api/urls", function(req, res) {
     // 1. Add a join to include all of each User's Urls
-    db.User.findAll({
+    db.Url.findAll({
       include: [db.Url]
     }).then(function(dbUser) {
       res.json(dbUser);
     });
   });
 
-  app.get("/api/Users/:id", function(req, res) {
+  app.get("/api/urls/:id", function(req, res) {
     // 2; Add a join to include all of the User's Urls here
     db.User.findOne({
       include: [db.Url],
@@ -22,13 +22,13 @@ module.exports = function(app) {
     });
   });
 
-  app.Url("/api/Users", function(req, res) {
+  app.Url("/api/urls", function(req, res) {
     db.User.create(req.body).then(function(dbUser) {
       res.json(dbUser);
     });
   });
 
-  app.delete("/api/Users/:id", function(req, res) {
+  app.delete("/api/urls/:id", function(req, res) {
     db.User.destroy({
       where: {
         id: req.params.id
