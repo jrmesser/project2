50d0902422e61ed93c418de19d148126ad0c3e1a
jrmesser <messer.jr@gmail.com>
2017-08-20 17:10:41 -0700

    added in a function to help when twitter length isn't present

--- controllers/scrape_html_metadata.js
+++ controllers/scrape_html_metadata.js
@@ -1,4 +1,6 @@
 var scrape = require('html-metadata');
+var wordCount = require('html-word-count');
+var request = require('request');
 
 module.exports = (url, callback) => {
     scrape(url).then(metadata => {
@@ -6,6 +8,25 @@ module.exports = (url, callback) => {
         urlObject.content_type = metadata.openGraph.type || "Website";
         urlObject.length = metadata.twitter.data1 ? parseInt(metadata.twitter.data1) : 0;
         urlObject.category = metadata.jsonLd["@type"] || "Web page";
-        callback(urlObject);
+        if (urlObject.length === 0 ) {
+            getLength(url, function(length) {
+                urlObject.length = length;
+                console.log(urlObject);
+            });
+        }
+        else {
+            callback(urlObject);
+        }
+
+    });
+};
+
+function getLength(url, callback) {
+    request(url, function (error, response, body) {
+//        console.log('error:', error); // Print the error if one occurred
+        const count = wordCount(body);
+        // adults read about 200 wpm according to https://www.irisreading.com/the-average-reading-speed/
+        const rate = 200;
+        callback(parseInt(Math.ceil(count/rate)));
     });
 };
