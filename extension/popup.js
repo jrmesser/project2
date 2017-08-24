2ddd7375a925ffa9dc0b04aba0dd850496c88862
jrmesser <messer.jr@gmail.com>
2017-08-23 19:08:00 -0700

    another stash

--- extension/popup.js
+++ extension/popup.js
@@ -117,3 +117,29 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
+
+//import scrape from 'html-metadata';
+import wordCount from 'html-word-count';
+
+// var scrape_data = function(url, callback){
+//     scrape(url).then(metadata => {
+//         var urlObject = {url: url};
+//         urlObject.content_type = metadata.openGraph.type || "Website";
+//         urlObject.category = metadata.jsonLd["@type"] || "Web page";
+
+//         getLength(url, function(length) {
+//             urlObject.length = length;
+//             console.log(urlObject);
+//         });
+//     });
+// };
+
+// function getLength(url, callback) {
+//     request(url, function (error, response, body) {
+//         //        console.log('error:', error); // Print the error if one occurred
+//         const count = wordCount(body);
+//         // adults read about 200 wpm according to https://www.irisreading.com/the-average-reading-speed/
+//         const rate = 200;
+//         callback(parseInt(Math.ceil(count/rate)));
+//     });
+// };
