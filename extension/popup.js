f8332dd92f0deee0666059fcd1932c659aec2950
jrmesser <messer.jr@gmail.com>
2017-08-24 06:48:42 -0700

    stashing changes

--- extension/popup.js
+++ extension/popup.js
@@ -27,7 +27,7 @@ function getCurrentTabUrl(callback) {
 
     // A tab is a plain object that provides information about the tab.
     // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
+      var url = tab.url;
 
     // tab.url is only available if the "activeTab" permission is declared.
     // If you want to see the URL of other tabs (e.g. after removing active:true
@@ -90,10 +90,35 @@ function getImageUrl(searchTerm, callback, errorCallback) {
 }
 
 function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("submit").addEventListener("click", function() {
+        console.log("submitting");
+        var sessionId;
+        chrome.cookies.get({url: "http://localhost:3000/", name: "sessionId"}, function(cookie) {
+            sessionId = cookie.value;
+            var data = {};
+            data.url = document.getElementById("url").value;
+            data.content_type = document.getElementById("content_type").value;
+            data.length = document.getElementById("length").value;
+            data.category = document.getElementById("category").value;
+            console.log("sessionId:", sessionId);
+            console.log("data", data);
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("status").innerHTML =
+                        this.responseText;
+                }
+            };
+            xhttp.open("POST", "http://localhost:3000/api/urls" + sessionId, true);
+            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xhttp.send(JSON.stringify(data));
+
+        });
+    });
+
   getCurrentTabUrl(function(url) {
     // Put the image URL in Google search.
     renderStatus('Performing Google Image search for ' + url);
@@ -118,10 +143,16 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
-//import scrape from 'html-metadata';
-import wordCount from 'html-word-count';
+// //import scrape from 'html-metadata';
+// import wordCount from 'html-word-count';
 
-// var scrape_data = function(url, callback){
+// var scrape_data = function(tab, callback){
+//     var html;
+
+//     chrome.pageCapture.saveAsMHTML(tab.id, function (mhtmlData) {
+//         console.log(mhtmlData); 
+//     });
+
 //     scrape(url).then(metadata => {
 //         var urlObject = {url: url};
 //         urlObject.content_type = metadata.openGraph.type || "Website";
@@ -143,3 +174,4 @@ import wordCount from 'html-word-count';
 //         callback(parseInt(Math.ceil(count/rate)));
 //     });
 // };
+
